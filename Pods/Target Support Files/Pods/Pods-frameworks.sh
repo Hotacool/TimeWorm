7b98a5a5669bfac3a225fee25b98b34996e9b720
sicong.qian <macbook@Kevin-Macbook-Pro.local>
2016-09-14 13:08:36 +0800

    + reveal

--- Pods/Target Support Files/Pods/Pods-frameworks.sh
+++ Pods/Target Support Files/Pods/Pods-frameworks.sh
@@ -82,3 +82,10 @@ strip_invalid_archs() {
   fi
 }
 
+
+if [[ "$CONFIGURATION" == "Debug" ]]; then
+  install_framework "${PODS_ROOT}/Reveal-SDK/RevealServer-2/iOS/RevealServer.framework"
+fi
+if [[ "$CONFIGURATION" == "Release" ]]; then
+  install_framework "${PODS_ROOT}/Reveal-SDK/RevealServer-2/iOS/RevealServer.framework"
+fi
